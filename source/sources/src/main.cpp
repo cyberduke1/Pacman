1a4e00c67ffc1f146741d54d6d0377c2b94fbb22
luvozulucybertech1 <2567454@students.wits.ac.za>
2024-05-02 12:09:00 +0200

    rendered

--- source/sources/src/main.cpp
+++ source/sources/src/main.cpp
@@ -41,6 +41,10 @@ int main(int argc, char *argv[])
 
     MainWindow.InitialLoad(renderer);
 
+    // Initial position of Pacman
+    int pacmanX = 11; // Column index
+    int pacmanY = 7;  // Row index
+
     bool quit = false;
     SDL_Event e;
 
@@ -52,10 +56,45 @@ int main(int argc, char *argv[])
             {
                 quit = true;
             }
+            else if (e.type == SDL_KEYDOWN)
+            {
+                // Handle key presses to move Pacman
+                switch (e.key.keysym.sym)
+                {
+                case SDLK_UP:
+                    if (pacmanY > 0 && map[pacmanY - 1][pacmanX] != '#')
+                    {
+                        --pacmanY;
+                    }
+                    break;
+                case SDLK_DOWN:
+                    if (pacmanY < map.size() - 1 && map[pacmanY + 1][pacmanX] != '#')
+                    {
+                        ++pacmanY;
+                    }
+                    break;
+                case SDLK_LEFT:
+                    if (pacmanX > 0 && map[pacmanY][pacmanX - 1] != '#')
+                    {
+                        --pacmanX;
+                    }
+                    break;
+                case SDLK_RIGHT:
+                    if (pacmanX < map[pacmanY].size() - 1 && map[pacmanY][pacmanX + 1] != '#')
+                    {
+                        ++pacmanX;
+                    }
+                    break;
+                }
+            }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
+        
+        // Update map with Pacman's new position
+        map[7] = map[7].substr(0, pacmanX) + '9' + map[7].substr(pacmanX + 1);
+
         if (!MainWindow.DrawMap(renderer, map))
         {
             break;
