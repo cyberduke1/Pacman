978a604f59e1811cfdd464e11bfaf84d027b8e07
luvozulucybertech1 <2567454@students.wits.ac.za>
2024-05-19 23:34:21 +0200

    new implementation

--- files/src/main.cpp
+++ files/src/main.cpp
@@ -89,9 +89,7 @@ int main(int argc, char* argv[]) {
             delay();
             Uint32 CurrentTime = SDL_GetTicks();
             DeltaTime = (CurrentTime - PreviousFrameTime) / 1000.0f;
-            PreviousFrameTime = CurrentTime;
-
-           
+            PreviousFrameTime = CurrentTime;           
         }
             if (CheckWall(UpdatedState, Pacman))
                 nextSprite = HandlePacmanMovement(Pacman, UpdatedState, nextSprite, DeltaTime);
@@ -100,7 +98,6 @@ int main(int argc, char* argv[]) {
             DrawMap(MainRenderer, UpdatedState, Pacman, LoadMaps, nextSprite);
             SDL_SetRenderDrawColor(MainRenderer, 0, 0, 0, 255);
             SDL_RenderPresent(MainRenderer);
-
     }
 
     return 0;
@@ -111,8 +108,8 @@ void Init() {
     GameWindow = SDL_CreateWindow("Pacman", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
     MainRenderer = SDL_CreateRenderer(GameWindow, -1, SDL_RENDERER_ACCELERATED);
 
-    std::string const name = "C:\\Users\\zulul\\Documents\\pacman\\pacman\\files\\resources\\sprites\\icon.png";
-    SDL_Surface* iconSurface = IMG_Load(name.c_str());
+    std::string const names = "C:\\Users\\zulul\\Documents\\pacman\\pacman\\files\\resources\\sprites\\icon.png";
+    SDL_Surface* iconSurface = IMG_Load(names.c_str());
     if (iconSurface == nullptr) {
         printf("Failed to load window icon image! SDL_ERROR: %s\n", SDL_GetError());
         return;
